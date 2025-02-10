a44b5749c96d8186b3ae4637aabd80814bbe6771
Ayush Pratap <ayushprince110@gmail.com>
2025-02-10 17:05:59 +0530

    learnt about react hooks and implemented useState()

---
+++ README.md
@@ -0,0 +1,19 @@
+/*
+Header
+    -Logo
+    -Nav Items
+Body
+    -Search
+    -Resturant container
+        -Resturant card
+            -Image
+            -Name of Resturant
+            -Star Rating
+            -Couisines
+            -delivery-time
+Footer
+    -Copyright
+    -Links
+    -Address
+    -Contact
+*/
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import restaurantList from "../API/restaurantList";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState(restaurantList);
     return (
       <div className="body">
-        <div className="search">Search</div>
+        <div className="filter">
+          <button
+            className="filter-btn"
+            onClick={() => {
+              const filteredList = listOfRestaurant.filter(
+                (res) => res.info.avgRating > 4.5
+              );
+              setListOfRestaurant(filteredList);
+              console.log(listOfRestaurant);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
         <div className="res-container">
-          {restaurantList.map((restaurant, index) => (
+          {listOfRestaurant.map((restaurant, index) => (
             <RestaurantCard key={index} restaurantData={restaurant} />
           ))}
         </div>
