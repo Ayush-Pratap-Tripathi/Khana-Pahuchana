c486b13da6b78a44dbda249aa94dd2bc80e07610
Ayush Pratap <ayushprince110@gmail.com>
2025-03-19 13:00:55 +0530

    tried to fetch swiggy's api while bypassing the CORS restrictions using a CORS proxy

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import restaurantList from "../API/restaurantList";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
   const [listOfRestaurant, setListOfRestaurant] = useState(restaurantList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await fetch(
+      `https://api.allorigins.win/get?url=${encodeURIComponent("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.569482&lng=85.5220799&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")}`  
+    );
+// parse the response's contents to json as it is returned in the form of string and it actually contains data from swiggy's api
+    const json = await response.json();
+    const data = JSON.parse(json.contents);
+
+    console.log(data);
+  };
+  
     return (
       <div className="body">
         <div className="filter">
